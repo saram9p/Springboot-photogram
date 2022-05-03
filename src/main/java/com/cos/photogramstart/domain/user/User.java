c6c285d7d59f829738c3cd83fc03358316e538d4
saram <saram9p@gmail.com>
2022-05-03 23:33:47 +0900

    회원가입 - User 모델 만들기

---
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -0,0 +1,45 @@
+package com.cos.photogramstart.domain.user;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// JPA - Java Persistence API (자바로 데이터를 영구적으로 저장(DB)할 수 있는 API를 제공)
+
+@AllArgsConstructor // 전체 생성자
+@NoArgsConstructor // 빈 생성자
+@Data
+@Entity // DB에 테이블을 생성
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
+	private  int id; // 서비스하는 프로그램이 아니라서 int로 함
+	
+	private String username;
+	private String password;
+	
+	private String name; // 이름
+	private String website; // 웹 사이트
+	private String bio; // 자기 소개
+	private String email; // 이메일
+	private String phone; // 전화번호
+	private String gender; // 성별
+	
+	private String profileimageUrl; // 사진
+	private String role; // 권한
+	
+	private LocalDateTime createDate;
+	
+	@PrePersist // DB에 INSERT 되기 직전에 실행
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
+}
--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,12 +1,20 @@
 package com.cos.photogramstart.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cos.photogramstart.web.dto.auth.SignupDto;
+
 @Controller // 1. IoC 2. 파일을 리턴하는 컨트롤러
 public class AuthController {
 
+	
+	private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+	
 	@GetMapping("/auth/signin")
 	public String signinForm() {
 		return "auth/signin";
@@ -20,7 +28,8 @@ public class AuthController {
 	// 회원가입을 진행함
 	// 회원가입버튼 -> /auth/signup -> /auth/signin
 	@PostMapping("/auth/signup")
-	public String signup() {
+	public String signup(SignupDto signupDto) { // key=value (x-www-form-urlencoded)
+		log.info(signupDto.toString());
 		return "auth/signin";
 	}
 }
