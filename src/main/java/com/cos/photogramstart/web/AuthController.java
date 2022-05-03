c246a5c54fd1d31d97261b51bd6902fb5719907f
saram <saram9p@gmail.com>
2022-05-04 00:17:59 +0900

    회원가입 - 완료

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -6,14 +6,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor // final 필드를 DI 할때 사용, final이 걸려있는 모든 것들에 대한 생성자를 만들어준다.
 @Controller // 1. IoC 2. 파일을 리턴하는 컨트롤러
 public class AuthController {
 
 	
 	private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 
+	private final AuthService authService; // final이 걸려있으면 무조건 생성자가 실행될때 혹은 객체가 만들어질 때 초기화를 해줘야한다.
+	
+//	public AuthController(AuthService authService) {
+//		this.authService = authService;
+//	}
 	
 	@GetMapping("/auth/signin")
 	public String signinForm() {
@@ -30,6 +40,11 @@ public class AuthController {
 	@PostMapping("/auth/signup")
 	public String signup(SignupDto signupDto) { // key=value (x-www-form-urlencoded)
 		log.info(signupDto.toString());
+		// User <- SignupDto
+		User user = signupDto.toEntity();
+		log.info(user.toString());
+		User userEntity = authService.회원가입(user);
+		System.out.println(userEntity);
 		return "auth/signin";
 	}
 }
--- src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.web.dto.auth;
 
+import com.cos.photogramstart.domain.user.User;
+
 import lombok.Data;
 
 @Data // Getter,Setter
@@ -8,4 +10,13 @@ public class SignupDto {
 	private String password;
 	private String email;
 	private String name;
+	
+	public User toEntity() { // 위의 네개의 데이터를 기반으로 User 객체를 만들어서 리턴한다.
+		return User.builder()
+				.username(username)
+				.password(password)
+				.email(email)
+				.name(name)
+				.build();
+	}
 }
