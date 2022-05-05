a9278abf577fbc4fead16b9c86220bad67f452a9
saram <saram9p@gmail.com>
2022-05-05 19:10:15 +0900

    공통 응답 DTO 만들기

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController // 데이터를 리턴하는 컨트롤러
 @ControllerAdvice // 모든 exception을 다 낚아챈다.
 public class ControllerExceptionHandler {
 
 	@ExceptionHandler(CustomValidationException.class) // RuntimeException이 발동하는 모든 Exception을 이 함수가 가로챈다. 
-	public Map<String, String> vaildationException(CustomValidationException e) {
-		return e.getErrorMap();
+	public CMRespDto<?> vaildationException(CustomValidationException e) {
+		return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
 	}
 }
