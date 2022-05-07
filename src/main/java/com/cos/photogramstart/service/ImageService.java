e889dd6c496d8e3ed598c6df50935e8d4a948335
saram <saram9p@gmail.com>
2022-05-08 00:58:38 +0900

    ﻿프로필 페이지 - Image DB에 업로드하기

--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.Image;
 import com.cos.photogramstart.domain.image.ImageRepository;
 import com.cos.photogramstart.web.dto.image.ImageUploadDto;
 
@@ -36,5 +37,12 @@ public class ImageService {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		// image 테이블에 저장
+		Image image = imageUploadDto.toEntity(imageFileName, principalDetails.getUser()); // 5e3beb68-bfdb-4d45-852b-00eba6088b73_2.jpg
+		Image imageEntity = imageRepository.save(image);
+		
+		System.out.println(imageEntity);
+		
 	}
 }
