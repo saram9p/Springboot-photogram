85b195350798a73f875cfbf8861fb371469bec9c
saram <saram9p@gmail.com>
2022-05-09 19:14:12 +0900

    구독정보 뷰 렌더링 - 구독정보 완성하기

--- src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
+++ src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
@@ -13,4 +13,11 @@ public interface SubscribeRepository extends JpaRepository<Subscribe, Integer> {
 	@Modifying // INSERT, DELETE, UPDATE 를 네이티브 쿼리로 작성하려면 해당 어노테이션 필요!!
 	@Query(value = "DELETE FROM subscribe WHERE fromUserId = :fromUserId AND toUserId = :toUserId", nativeQuery = true)
 	void mUnSubscribe(int fromUserId, int toUserId);
+	
+	@Query(value = "SELECT COUNT(*) FROM subscribe WHERE fromUserId = :principalId AND toUserId = :pageUserId", nativeQuery = true)
+	int mSubscribeState(int principalId, int pageUserId);
+	
+	// 결과 1이면 해당 페이지의 주인이 구독한 구독자의 수를 나타낸다.
+	@Query(value = "SELECT COUNT(*) FROM subscribe WHERE fromUserId = :pageUserId", nativeQuery = true)
+	int mSubscribeCount(int pageUserId);
 }
--- src/main/java/com/cos/photogramstart/web/dto/user/UserProfileDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/user/UserProfileDto.java
@@ -14,5 +14,7 @@ import lombok.NoArgsConstructor;
 public class UserProfileDto {
 	private boolean pageOwnerState; // 이 페이지의 주인인지 아닌지에 대한 여부 데이터.
 	private int imageCount;
+	private boolean subscribeState; // 구독 상태
+	private int subscribeCount; // 구독자 수
 	private User user;
 }
