50c6d258a7c73bb5a7cd89eefc050894f8c0da91
saram <saram9p@gmail.com>
2022-05-10 23:25:03 +0900

    구독정보 뷰 렌더링 - 구독 모달 정보에서 구독 및 구독취소 구현하기

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -72,9 +72,9 @@ function getSubscribeModalItem(u) {
 		<div class="subscribe__btn">`;
 	if (!u.equalUserState) { // 동일 유저가 아닐 때 버튼이 만들어져야함
 		if (u.subscribeState) { // 구독한 상태
-			item += `<button class="cta blue" onclick="toggleSubscribeModal(this)">구독취소</button>`;
+			item += `<button class="cta blue" onclick="toggleSubscribe(${u.id}, this)">구독취소</button>`;
 		} else { // 구독안한 상태
-			item += `<button class="cta" onclick="toggleSubscribeModal(this)">구독하기</button>`;
+			item += `<button class="cta" onclick="toggleSubscribe(${u.id}, this)">구독하기</button>`;
 		}
 	}
 	item += `
@@ -84,19 +84,7 @@ function getSubscribeModalItem(u) {
 	return item;
 }
 
-
-// (3) 구독자 정보 모달에서 구독하기, 구독취소
-function toggleSubscribeModal(obj) {
-	if ($(obj).text() === "구독취소") {
-		$(obj).text("구독하기");
-		$(obj).toggleClass("blue");
-	} else {
-		$(obj).text("구독취소");
-		$(obj).toggleClass("blue");
-	}
-}
-
-// (4) 유저 프로파일 사진 변경 (완)
+// (3) 유저 프로파일 사진 변경 (완)
 function profileImageUpload() {
 	$("#userProfileImageInput").click();
 
@@ -118,7 +106,7 @@ function profileImageUpload() {
 }
 
 
-// (5) 사용자 정보 메뉴 열기 닫기
+// (4) 사용자 정보 메뉴 열기 닫기
 function popup(obj) {
 	$(obj).css("display", "flex");
 }
@@ -128,17 +116,17 @@ function closePopup(obj) {
 }
 
 
-// (6) 사용자 정보(회원정보, 로그아웃, 닫기) 모달
+// (5) 사용자 정보(회원정보, 로그아웃, 닫기) 모달
 function modalInfo() {
 	$(".modal-info").css("display", "none");
 }
 
-// (7) 사용자 프로파일 이미지 메뉴(사진업로드, 취소) 모달
+// (6) 사용자 프로파일 이미지 메뉴(사진업로드, 취소) 모달
 function modalImage() {
 	$(".modal-image").css("display", "none");
 }
 
-// (8) 구독자 정보 모달 닫기
+// (7) 구독자 정보 모달 닫기
 function modalClose() {
 	$(".modal-subscribe").css("display", "none");
 	location.reload();
